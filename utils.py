59a7bfc39a847849b39bc29dc23948a694e0408b
RuoyuChen10 <2389388826@qq.com>
2021-02-08 04:26:03 +0800

    update

--- utils.py
+++ utils.py
@@ -20,6 +20,17 @@ def get_network(command,weight_path=None):
     # Load model
     if weight_path is not None and os.path.exists(weight_path):
         model = torch.load(weight_path)
+        try:
+            # if multi-gpu model:
+            model = model.module
+        except:
+            # just 1 gpu or cpu
+            pass
+        pretrain = model.state_dict()
+        new_state_dict = {}
+        for k,v in pretrain.items():
+            new_state_dict[k] = v
+        model.load_state_dict(new_state_dict, strict=True)
         print("Model parameters: " + weight_path + " has been load!")
         return model
     elif command == "resnet50":
@@ -33,7 +44,7 @@ def get_network(command,weight_path=None):
         with open(weight_path, 'rb') as f:
             obj = f.read()
             weights = {key: torch.from_numpy(arr) for key, arr in pickle.loads(obj, encoding='latin1').items()}
-        net.load_state_dict(weights)
+        net.load_state_dict(weights, strict=True)
         return net
     
 
